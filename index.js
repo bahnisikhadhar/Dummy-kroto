108be43f7e7e2b65a43bd4e87474639b1fe697e8
bahnisikha <bahnisikhadhar@gmail.com>
2022-11-17 13:38:53 +0530

    made background slider using DOM

---
+++ index.js
@@ -0,0 +1,12 @@
+const leftSide=document.getElementById("slide");
+
+function slider(event) 
+{
+    console.log(event);
+    const {screenX}=event;
+    leftSide.style.width= `${screenX}px`;
+    // leftSide.style.width = `${event.clientX / window.innerWidth * 100}%` ;-----> this one is also correct
+}
+
+document.addEventListener("mousemove",slider);
+
