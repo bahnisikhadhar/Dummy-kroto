3f17c3f6feda3284bf6ba745619ba20caa3f4f1b
bahnisikha <bahnisikhadhar@gmail.com>
2023-05-26 10:00:31 +0530

    small changes made

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const leftSide=document.getElementById("slide");
 
 function slider(event) 
 {
-    console.log(event);
+    // console.log(event);
     const {screenX}=event;
     leftSide.style.width= `${screenX}px`;
     // leftSide.style.width = `${event.clientX / window.innerWidth * 100}%` ;-----> this one is also correct
